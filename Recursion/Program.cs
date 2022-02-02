59d22171373cb7e8fac185adbb4dbd6c851b5b81
hazardhawkk <shahzaibzafarshazi@gmail.com>
2022-02-02 11:34:43 +0000

    Add project files.

---
+++ Recursion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecursiveReverseString("Trees are cool"); 
+        }
+        static int RecursiveFactorial(int n)
+        {
+            if(n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n* RecursiveFactorial(n-1);
+            }
+
+        }
+
+        static void RecursiveReverseString(string str)  
+        {
+            if (str.Length <= 1)        //this is barely my own code but i understood it
+            {
+                Console.Write(str);
+            } else
+            {
+                Console.Write(str[str.Length - 1]);
+                RecursiveReverseString(str.Substring(0, str.Length - 1));
+            }
+        }
+    }
+}
